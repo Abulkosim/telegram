e71e89655532ef215a802b29ae997f09eecc90e2
Abulkosim <abulkasimkattaev@gmail.com>
2024-08-26 23:48:52 +0500

    refactor: setup next steps

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, {Application, Request, Response, NextFunction} from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import {config} from './config/environment';
+
+const app: Application = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Telegram-like App API');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
-import express, {Application} from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
+import app from './app';
+import { config } from './config/environment';
+import mongoose from 'mongoose';
 
-dotenv.config();
+const PORT = config.port || 3000;
 
-const app: Application = express();
-
-app.use(cors());
-app.use(express.json());
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+mongoose.connect(config.mongoUri as string)
+    .then(() => {
+        console.log('Connected to MongoDB');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+        process.exit(1);
+    });
